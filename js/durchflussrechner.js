53897af98fec5eb032c691492e32c7ebaca9b1ad
samir <samir@192.168.32.75>
2013-05-17 18:32:50 +0200

    ein paar kleine anpassungen, Framework muss noch festgelegt werden

---
+++ js/durchflussrechner.js
@@ -0,0 +1,17 @@
+;(function ( $, window, document, undefined ) {
+  
+  var Durchflussrechner = function(){
+    
+    Durchflussrechner.allclear = function(){
+      alert('Wow, I am In!');
+    };
+    
+  };
+  
+  
+  
+  
+  $(function(){
+    
+  });
+})( jQuery, window, document );
